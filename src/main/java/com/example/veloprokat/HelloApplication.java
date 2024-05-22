f00bbf4b18b2e37ee9283503fa03ad1fd82892d4
София Ворошнина <voroshnina.sofia31052005@yandex.ru>
2024-05-22 15:14:19 +0300

    Графика окошек(часть 2)

--- src/main/java/com/example/veloprokat/HelloApplication.java
+++ src/main/java/com/example/veloprokat/HelloApplication.java
@@ -11,10 +11,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 612, 200);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        FXMLLoader loader = new FXMLLoader(Bikes.class.getResource("bikes.fxml")); // create and load() view
+        stage.show();
+        stage.setScene(scene);
     }
 
     public static void main(String[] args) {
